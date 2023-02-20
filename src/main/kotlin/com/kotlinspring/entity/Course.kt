3cb8b7a0cd67b0cb7ba82cbaccaa70f376032c59
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-02-20 04:24:44 +0100

    added h2 database configuration

--- src/main/kotlin/com/kotlinspring/entity/Course.kt
+++ src/main/kotlin/com/kotlinspring/entity/Course.kt
@@ -1,14 +1,13 @@
 package com.kotlinspring.entity
 
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "Courses")
 data class Course (
         @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
         val id : Int?,
         val name: String,
         val category: String
